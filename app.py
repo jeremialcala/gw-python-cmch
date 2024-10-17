cd623e9db93a6ec782a61f7521de760d7ab9a971
Abraham Gómez <moisesdelciervo@gmail.com>
2024-10-17 14:25:37 -0400

    se agrega endpoints y clases dto

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ from starlette.concurrency import iterate_in_threadpool
 from fastapi import FastAPI, Request, Response
 from fastapi import status
 from inspect import currentframe
+from fastapi.encoders import jsonable_encoder
 
+from dto import Persona
+from dto import Nomina
+from dto import Unidad
+from dto import Beneficio
 
+from dto import Producto
+from dto import Usuario
 from classes import Settings
 
 app = FastAPI(
@@ -18,75 +25,122 @@ app = FastAPI(
 )
 _set = Settings()
 
-@app.post('/personas', tags=['personas'])
 
-@app.get('/personas/{id_persona}', tags=['personas'])
+@app.post('/personas', tags=['personas'])
+async def registrar_persona(persona: Persona):
+    data = persona
+    Persona.append(jsonable_encoder(data))
+    return Persona
+
+@app.get('/personas/{cedula}', tags=['personas'])
+async def consultar_persona(cedula:int):
+    return Response(status_code=200)
+    pass
 
 @app.put('/personas/{id_persona}', tags=['personas'])
-async def update_persona(request:Request):
+async def actualizar_persona(id_persona: int):
     return Response(status_code=200)
+    pass
 
 @app.delete('/personas/{id_persona}', tags=['personas'])
-async def delete_persona(request:Request):
+async def eliminar_persona(id_persona: int):
     return Response(status_code=200)
+    pass
 
 
 
 @app.post('/nomina', tags=['nominas'])
+async def registrar_nomina(nomina: Nomina):
+    return Response(status_code=200)
+    pass
 
 @app.get('/nominas/{codnom}', tags=['nominas'])
+async def consultar_nomina(codnom: int):
+    return Response(status_code=200)
 
 @app.put('/nominas/{codnom}', tags=['nominas'])
+async def actualizar_nomina(codnom: int):
+    return Response(status_code=200)
 
 @app.delete('/nominas/{codnom}', tags=['nominas'])
-
+async def eliminar_nomina(codnom:int):
+    return Response(status_code=200)
 
 
 
 
 @app.post('/unidades', tags=['unidades'])
+async def registrar_unidad(unidad: Unidad):
+    return Response(status_code=200)
 
 @app.get('/unidades/{id_und}', tags=['unidades'])
+async def consultar_unidad(id_und: int):
+    pass
 
 @app.put('/unidades/{id_und}', tags=['unidades'])
+async def actualizar_unidad(id_und: int):
+    pass
 
 @app.delete('/unidades/{id_und}', tags=['unidades'])
-
-
+async def eliminar_unidad(id_und: int):
+    pass
 
 
 
 @app.post('/productos', tags=['productos'])
+async def registrar_producto(producto: Producto):
+    return Response(status_code=200)
 
 @app.get('/productos/{id_producto}', tags=['productos'])
+async def consultar_producto(id_producto: int):
+    return Response(status_code=200)
 
 @app.put('/productos/{id_producto}', tags=['productos'])
+async def actualizar_unidad(id_producto: int):
+    return Response(status_code=200)
 
 @app.delete('/productos/{id_producto}', tags=['productos'])
-
+async def eliminar_producto(id_producto: int):
+    return Response(status_code=200)
 
 
 
 
 @app.post('/beneficios', tags=['beneficios'])
+async def registrar_beneficio(benefico: Beneficio):
+    return Response(status_code=200)
 
-@app.get('/beneficios/{id_producto}', tags=['beneficios'])
+@app.get('/beneficios/{id_beneficio}', tags=['beneficios'])
+async def consultar_beneficio(id_beneficio: int):
+    return Response(status_code=200)
 
-@app.put('/beneficios/{id_producto}', tags=['beneficios'])
+@app.put('/beneficios/{id_beneficio}', tags=['beneficios'])
+async def actualizar_benefcio(id_beneficio: int):
+    return Response(status_code=200)
 
-@app.delete('/beneficios/{id_producto}', tags=['beneficios'])
+@app.delete('/beneficios/{id_beneficio}', tags=['beneficios'])
+async def eliminar_beneficio(id_beneficio: int):
+    return Response(status_code=200)
+
 
 
 
 
 @app.post('/seguridad', tags=['seguridad'])
+async def registrar_usuaro(usuario: Usuario):
+    return Response(status_code=200)
 
 @app.get('/seguridad/{user}', tags=['seguridad'])
+async def consultar_usuario(user: str):
+    return Response(status_code=200)
 
 @app.put('/seguridad/{user}', tags=['seguridad'])
+async def restablecer_clave(user: str):
+    return Response(status_code=200)
 
-@app.delete('/seguridad/{user}', tags=['seguridad'])
-
+@app.delete('/seguridad/{id_user}', tags=['seguridad'])
+async def eliminar_usuario(id_usuario: int):
+    return Response(status_code=200)
 
 
 
@@ -98,4 +152,3 @@ async def registrar_persona(request:Request):
     """
     data=await request.json()
     return Response(status_code=200)
-
---
+++ dto.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class Persona(BaseModel):
+    id_persona: int = Field(min_length=1)
+    cedula: int = Field(min_length=6,max_length=8)
+    nombre: str = Field(min_length=7, max_length=15)
+    apellido: str = Field(min_length=7, max_length=40)
+    estatus: int = Field(min_length=1)
+    fecha_nac : date
+    fecha_ingreso: date
+
+class Nomina(BaseModel):
+    id_nomina: int = Field(min_length=1)
+    codnom: int = Field(defalt="001", min_length=3,max_length=3)
+    descripcion: str = Field(default="Nómina de", min_length= 10, max_length=50)
+    estatus: int = Field(min_length=1)
+
+class Unidad(BaseModel):
+    id_unidad: int = Field(min_length=1)
+    descripcion: str = Field(default="Nómina de", min_length= 10, max_length=50)
+    estatus: int = Field(min_length=1)
+
+class Producto(BaseModel):
+    id_producto: int = Field(min_length=1)
+    descripcion: str = Field(default="Nómina de", min_length= 10, max_length=50)
+    estatus: int = Field(min_length=1)
+
+class Beneficio(BaseModel):
+    id_beneficio: int = Field(min_length=1)
+    fecha_entrega: date
+    tipo_benefico: int = Field(min_length=1)
+    estatus: int = Field(min_length=1)
+
+class Usuario(BaseModel):
+    id_usuario: int = Field(min_length=1)
+    username: str = Field(min_length= 5, max_length=10)
+    password: str = Field(min_length= 5, max_length=10)
+    fecha_registro: date
+    estatus: int = Field(min_length=1)
