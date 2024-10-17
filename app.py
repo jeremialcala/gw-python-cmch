d6809ff044fb01a5673a5ad94a3870f41fe8e1bd
Abraham Gómez <moisesdelciervo@gmail.com>
2024-10-17 12:11:33 -0400

    cambios en app

--- app.py
+++ app.py
@@ -18,18 +18,7 @@ app = FastAPI(
 )
 _set = Settings()
 
-@app.get('/')
-def mensaje():
-    return 'Hola'
-
-
 @app.post('/personas', tags=['personas'])
-async def registrar_persona(request:Request):
-    """
-    
-    """
-    data=await request.json()
-    return Response(status_code=200)
 
 @app.get('/personas/{id_persona}', tags=['personas'])
 
@@ -41,4 +30,72 @@ async def update_persona(request:Request):
 async def delete_persona(request:Request):
     return Response(status_code=200)
 
+
+
+@app.post('/nomina', tags=['nominas'])
+
+@app.get('/nominas/{codnom}', tags=['nominas'])
+
+@app.put('/nominas/{codnom}', tags=['nominas'])
+
+@app.delete('/nominas/{codnom}', tags=['nominas'])
+
+
+
+
+
+@app.post('/unidades', tags=['unidades'])
+
+@app.get('/unidades/{id_und}', tags=['unidades'])
+
+@app.put('/unidades/{id_und}', tags=['unidades'])
+
+@app.delete('/unidades/{id_und}', tags=['unidades'])
+
+
+
+
+
+@app.post('/productos', tags=['productos'])
+
+@app.get('/productos/{id_producto}', tags=['productos'])
+
+@app.put('/productos/{id_producto}', tags=['productos'])
+
+@app.delete('/productos/{id_producto}', tags=['productos'])
+
+
+
+
+
+@app.post('/beneficios', tags=['beneficios'])
+
+@app.get('/beneficios/{id_producto}', tags=['beneficios'])
+
+@app.put('/beneficios/{id_producto}', tags=['beneficios'])
+
+@app.delete('/beneficios/{id_producto}', tags=['beneficios'])
+
+
+
+
+@app.post('/seguridad', tags=['seguridad'])
+
+@app.get('/seguridad/{user}', tags=['seguridad'])
+
+@app.put('/seguridad/{user}', tags=['seguridad'])
+
+@app.delete('/seguridad/{user}', tags=['seguridad'])
+
+
+
+
+
+@app.post('/personas', tags=['otros'])
+async def registrar_persona(request:Request):
+    """
+    
+    """
+    data=await request.json()
+    return Response(status_code=200)
 
