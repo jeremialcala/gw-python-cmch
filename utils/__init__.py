dba5db6f1a3adcf51b190bb1233f316bfe9fe5a3
Jeremi Alcala <jeremialcala@gmail.com>
2024-11-24 22:00:04 -0400

    Update: Adding dynamic object creators to the __init__ for utils.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,5 @@
     This module have utilities methods for this project
 """
 from .logging import configure_logging
-
+from .general import documenting_parameter
+from .model import create_dynamic_dto_model, generate_properties, create_dynamic_orm_model, create_model, resource_from_model
